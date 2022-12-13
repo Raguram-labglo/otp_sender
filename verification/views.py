68ffc6f98cce26031fbde217a3ef56f1762594dc
Raguram-labglo <raguram.labglo@gmail.com>
2022-12-13 18:01:20 +0530

    Otp login sucessfully

--- verification/views.py
+++ verification/views.py
@@ -1,14 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 from .models import user_mobile
 from .serializers import Otp_serializer, Register, User, Otp_verifier
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
 from rest_framework.authtoken.models import Token
 from .helper import *
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
 
 class Register(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = Register
+    permission_classes = [AllowAny]
 
     def post(self, request):
 
@@ -24,15 +29,21 @@ class Register(ModelViewSet):
         token, li = Token.objects.get_or_create(user=user)
         return Response({'token': token.key})
 
-class Otp_checker(ModelViewSet):
-    queryset = user_mobile.objects.all()
+class Otp_checker(CreateAPIView):
+    
     serializer_class = Otp_verifier
-    http_method_names = ['post']
+    permission_classes = [AllowAny]
 
-    def perform_create(self, serializer):
+    def post(self, serializer):
+        
         user = User.objects.get(username = self.request.data['username'])
         print(user.password)
+        user.set_password(raw_password = user.password)
+        user.save
         otp_owner = user_mobile.objects.get(user = user.pk)
+        if otp_owner.isVerified == True:
+            print('------+++++++++++++++++++++++------------------------')
+            return Response({'details':'alredy verifide accout'}, status=status.HTTP_200_OK)
         given_otp = otp_owner.counter
         recived = self.request.data['recived_otp']
         if int(given_otp) == int(recived):
@@ -44,8 +55,6 @@ class Otp_checker(ModelViewSet):
             obj.send_otp_via_message()
             owner = authenticate(username = user.username, password = user.password)
             print(owner)
-            login(self.request, owner)
-            return Response({'detail':'otp ok'})
-        else:
-            print('no')
-            return Response({'detail':'otp ok'})
+            login(self.request, user)
+            token, li = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
